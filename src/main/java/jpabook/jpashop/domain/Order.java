6a06f31aebe00384cf8a2ffb4f1e8e8b1d6aa356
ckr3453 <ckr3453@gmail.com>
2022-08-29 18:14:35 +0900

    feat: 주문 엔티티 추가

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // 컬렉션 객체는 필드에서 초기화함으로써 NullPointerException 방지 (Best practice)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    // SpringPhysicalNamingStrategy 로 인해 order_date (스네이크 케이스)로 바뀜
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // 연관관계 편의 메서드 (양방향)
+    // set 혹은 get 할때 해당 필드의 연관관계에 속하는 객체도 동시에 초기화
+    // 연관관계의 주인 격인 도메인에서 작업
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+}
