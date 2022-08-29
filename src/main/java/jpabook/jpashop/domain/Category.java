184feb0c34a0ad3bed133d79c27116c553944e75
ckr3453 <ckr3453@gmail.com>
2022-08-29 18:07:30 +0900

    feat: 상품의 카테고리를 위한 엔티티 추가

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    // 컬렉션 객체는 필드에서 초기화함으로써 NullPointerException 방지 (Best practice)
+    @ManyToMany
+    @JoinTable(
+            name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    // 컬렉션 객체는 필드에서 초기화함으로써 NullPointerException 방지 (Best practice)
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    // 연관관계 편의 메서드
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+}
