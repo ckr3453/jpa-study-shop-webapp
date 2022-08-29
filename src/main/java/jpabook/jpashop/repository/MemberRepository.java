c0f4d27e2d5eed9e9d492f106e82cddcb81420de
ckr3453 <ckr3453@gmail.com>
2022-08-29 18:13:18 +0900

    remove: 회원 레포지토리 추가

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository // 데이터 접근 계층 (DAO)
+public class MemberRepository {
+
+    @PersistenceContext // 스프링에서 해당 객체를 EntityManager로 inject
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    // 일반적인 SQL이 아닌 JPQL (객체 대상)
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
