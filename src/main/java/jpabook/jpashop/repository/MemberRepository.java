0a1530b0465352d7e4b6eea1ece7e2c0fb465959
ckr3453 <ckr3453@gmail.com>
2022-08-29 22:47:56 +0900

    refactor(annotation): @PersistenceContext -> 어노테이션으로 대체

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository // 데이터 접근 계층 (DAO)
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext // 스프링에서 해당 객체를 EntityManager로 inject
-    private EntityManager em;
+//    @PersistenceContext 스프링에서 해당 객체를 EntityManager로 inject
+    private final EntityManager em;
 
     public void save(Member member){
         em.persist(member);
