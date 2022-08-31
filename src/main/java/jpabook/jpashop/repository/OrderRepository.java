c43a92e370e02a08bee7a21bbb144bd7c05e1205
ckr3453 <ckr3453@gmail.com>
2022-08-31 17:17:44 +0900

    feat: 상품주문 레포지토리 클래스 추가

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.JoinTable;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long orderId){
+        return em.find(Order.class, orderId);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch){
+
+        return em.createQuery("select o from Order o join o.member m", Order.class)
+                .setMaxResults(1000)
+                .getResultList();
+    }
+
+    /**
+     * @deprecated
+     * JPA Criteria 를 활용한 동적쿼리 생성 (JPA 표준이지만 유지보수성이 떨어지고 복잡하여 권장하지 않음)
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> query = cb.createQuery(Order.class);
+        Root<Order> o = query.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원 이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        query.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> cq = em.createQuery(query).setMaxResults(1000);
+        return cq.getResultList();
+    }
+}
