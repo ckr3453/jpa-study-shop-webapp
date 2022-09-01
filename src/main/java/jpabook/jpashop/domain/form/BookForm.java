328cf3345e6affccc949d2143e79d370f4b4b931
ckr3453 <ckr3453@gmail.com>
2022-09-01 18:20:32 +0900

    feat(class): DTO 역할 BookForm class 추가

---
+++ src/main/java/jpabook/jpashop/domain/form/BookForm.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    // 상품 공통 속성
+    @NotEmpty(message = "상품명은 필수입니다.")
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    // 책 속성
+    private String author;
+    private String isbn;
+}
