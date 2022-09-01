e05cb6252daa3640f42fcb3f7d5c038d39980dcb
ckr3453 <ckr3453@gmail.com>
2022-09-01 18:31:03 +0900

    Add: 변경감지 방식인 updateItem 메서드 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -20,6 +20,23 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    /**
+     * 변경감지기능 (dirty checking) 을 활용하여 정보 수정
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        // 영속 상태임 (영속성 컨텍스트가 관리 중)
+        Item item = itemRepository.findOne(itemId);
+
+        // 추가적으로 레포지토리에 저장할 필요 x, 자동으로 커밋, flush
+        // 속성을 선택하여 저장할 수 있음
+        // 예시로 setter 를 썼지만 setter 사용하지말고 엔티티 내에서 의미있는 메소드로 구현해야함. (setter 사용 시 추적이 힘듬)
+        item.setId(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+
     public List<Item> findItem() {
         return itemRepository.findAll();
     }
